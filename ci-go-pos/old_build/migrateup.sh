85c99a594e13261ee46e635a7cb67bd74959548e
Hoàng Hà Giang <163991278+LufDevOps@users.noreply.github.com>
2024-04-15 13:49:16 +0700

    Add files via upload
    first commit

---
+++ ci-go-pos/old_build/migrateup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Postgres is up - begin migrating"
+# Run migration up
+
+#docker exec -it postgres createdb --username=postgres --owner=postgres postgres
+
+migrate -path ./migration -database "postgresql://$POSTGRESQL_USER:$POSTGRESQL_PASSWORD@$POSTGRESQL_HOST:$POSTGRESQL_PORT/$POSTGRESQL_DB?sslmode=disable" -verbose up
